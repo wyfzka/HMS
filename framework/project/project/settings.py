2a480e8b6adaf19acba90eda664596086d98183d
faded61966 <2625353860@qq.com>
2020-03-31 09:54:06 +0800

    change the password of the database 'sunck' to '123456'

--- framework/project/project/settings.py
+++ framework/project/project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "sunck",
         'USER':'root',
-        'PASSWORD':'hdw662878',
+        'PASSWORD':'123456',
         'HOST':'localhost',
         'PORT':'3306',
     }
